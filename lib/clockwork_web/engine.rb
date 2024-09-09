e2d8affd46b98f6600021fbd3d69eb66d8113d9d
chaadow <chedli@hoggo.com>
2024-09-09 09:41:02 +0200

    Fix engine

--- lib/clockwork_web/engine.rb
+++ lib/clockwork_web/engine.rb
@@ -1,13 +1,10 @@
+module ClockworkWeb
+  class Engine < ::Rails::Engine
+    isolate_namespace ClockworkWeb
 
-if defined?(Rails)
-  module ClockworkWeb
-    class Engine < ::Rails::Engine
-      isolate_namespace ClockworkWeb
-  
-      initializer "clockwork_web" do
-        ClockworkWeb.clock_path ||= Rails.root.join("clock")
-        require ClockworkWeb.clock_path.to_s
-      end
+    initializer "clockwork_web" do
+      ClockworkWeb.clock_path ||= Rails.root.join("clock")
+      require ClockworkWeb.clock_path.to_s
     end
   end
 end
